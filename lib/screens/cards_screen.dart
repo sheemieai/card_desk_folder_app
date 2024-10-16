b43cbfc65a30da5e9e1d9a6dc7dfc65c2be015e5
Arib Husain <husainarib@gmail.com>
2024-10-16 18:17:51 -0400

    Add folder screen and start cards screen

---
+++ lib/screens/cards_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class CardsScreen extends StatelessWidget {
+  final String folderName;
+
+  CardsScreen({required this.folderName});
+  
+  @override
+  Widget build(BuildContext context) {
+    // Get the list of cards for the selected folder
+    final cards = cardsByFolder[folderName] ?? [];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$folderName Cards'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // Two cards per row
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0,
+            childAspectRatio:
+                0.8, // Adjust ratio to fit images and text properly
+          ),
+          itemCount: cards.length,
+          itemBuilder: (context, index) {
+            var card = cards[index];
+            return Card(
+              elevation: 4.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Card image
+                  Expanded(
+                    child: Image.asset(
+                      card['image'],
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  // Card name
+                  Text(
+                    card['name'],
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+      // Floating Action Button to add a new card (UI placeholder)
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add card functionality (UI placeholder)
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Add Card',
+      ),
+    );
+  }
+}
---
+++ lib/screens/folders_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'cards_screen.dart';
+
+class FoldersScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> folders = [
+    {'name': 'Hearts', 'image': 'lib/img/10_of_hearts.png', 'cardCount': 3},
+    {'name': 'Spades', 'image': 'lib/img/10_of_spades.png', 'cardCount': 5},
+    {'name': 'Diamonds', 'image': 'lib/img/10_of_diamonds.png', 'cardCount': 6},
+    {'name': 'Clubs', 'image': 'lib/img/10_of_clubs.png', 'cardCount': 4}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Folders'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0,
+            childAspectRatio: 0.8,
+          ),
+          itemCount: folders.length,
+          itemBuilder: (context, index) {
+            var folder = folders[index];
+            return GestureDetector(
+              onTap: () {
+                // Navigate to CardsScreen and pass the folder name
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        CardsScreen(folderName: folder['name']),
+                  ),
+                );
+              },
+              child: Card(
+                elevation: 4.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        folder['image'],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    // Folder name
+                    Text(
+                      folder['name'],
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    // Number of cards in the folder
+                    Text(
+                      '${folder['cardCount']} cards',
+                      style: const TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
